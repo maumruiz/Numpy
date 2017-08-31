7abdd6ab78374028380e35df168554fa55df1de3
maumruiz <mau.mruiz@hotmail.com>
2017-08-30 20:37:48 -0500

    Range and splice arrays

---
+++ arrays.py
@@ -0,0 +1,20 @@
+x = range(1,15)
+print "range(1,15):"
+print x
+
+print "range(5,100,5):"
+print range(5,100,5)
+
+print "range(10,-1,-1)"
+L = range(10,-1,-1)
+print L
+
+print "splice [2:8]"
+print L[2:8]
+
+print "splice [8:2:-1]"
+print L[8:2:-1]
+
+print "Reverse a List"
+print "L[::-1]"
+print L[::-1]
