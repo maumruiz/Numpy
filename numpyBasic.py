b75c58e04ecdba3faa08aeced676ce575dd41223
maumruiz <mau.mruiz@hotmail.com>
2017-09-03 23:53:35 -0500

    Numpy basics and pandas

--- numpyBasic.py
+++ numpyBasic.py
@@ -1,7 +1,8 @@
 import numpy as np
 
+print "Numpy Basics"
 x = np.array([1.0,2.,3.1])
-print x
+print "x = np.array([1.0,2.,3.1]) = " + str(x)
 
 print "x[2] = " + str(x[2])
 
@@ -22,4 +23,20 @@ print "a.dtype.name = " + str(a.dtype.name)
 print "a.itemsize = " + str(a.itemsize)
 print "a.size = " + str(a.size)
 print "type(a) = " + str(type(a))
+print ""
 
+# Two dimensional arrays
+print "----------"
+print "Two dimensional arrays"
+print ""
+
+a = np.arange(15).reshape(3,5)
+print "a = np.arange(15).reshape(3,5) :"
+print a
+
+b = np.arange(15).reshape(5, 3)
+print "b = np.arange(15).reshape(5, 3) :"
+print b
+
+print "b.transpose()"
+print b.transpose()
